44ed75932200afd2ea51d3de9ba50d54ab40e703
abse coding <absecoding@gmail.com>
2023-01-12 19:08:56 +0530

    First commit with Raahat project

---
+++ proj1/django_project/locallibrary/catalog/urls.py
@@ -0,0 +1,16 @@
+#from django.contrib import admin
+from django.urls import path
+from catalog import views
+
+#from django.shortcuts import include
+
+urlpatterns = [
+   # path('admin/', admin.site.urls),
+    #path('',include('catalog.views'))
+
+    #path('catalog/', views.catalog, name="catalog" ),
+    #path('trialpage/', views.trialpage, name="trialpage"),
+    
+
+
+]
